bdea5a386c7ed115b9fa07e93981c4ede58781ed
ddingmin <ddingminyt@gmail.com>
2022-07-12 18:15:51 +0900

    [Silver III] Title: Four Squares, Time: 148 ms, Memory: 114764 KB -BaekjoonHub

---
+++ 백준/Silver/17626. Four Squares/Four Squares.py
@@ -0,0 +1,13 @@
+n = int(input())
+d = [0] * (n + 1)
+d[0], d[1] = 0, 1
+
+for i in range(2, n + 1):
+    minValue = 1e9
+    j = 1
+    while (j ** 2) <= i:
+        minValue = min(minValue, d[i - (j ** 2)])
+        j += 1
+    d[i] = minValue + 1
+
+print(d[n])
