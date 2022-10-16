9f4a29fd72752aba6a9387e290b3bb6c4f613ed5
ddingmin <ddingminyt@gmail.com>
2022-10-16 23:55:00 +0900

    [Silver V] Title: 숫자 카드, Time: 652 ms, Memory: 214756 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -1,7 +1,7 @@
 n = input()
-card = set(list(map(int, input().split())))
+card = set(list(input().split()))
 t = input()
-arr = list(map(int, input().split()))
+arr = list(input().split())
 for i in arr:
     if i in card: print(1, end = " ")
     else: print(0, end = " ")
