7011d7c6b0fec34b011c75a5c2b86243fa718cd5
ddingmin <ddingminyt@gmail.com>
2022-12-04 20:10:28 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 448 ms, Memory: 133864 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -1,25 +1,41 @@
-import heapq
+# Kruskal
 input = __import__('sys').stdin.readline
 
 V, E = map(int, input().split())
-adj = [[] for _ in range(V + 1)]
-
+edges = []
 for _ in range(E):
     a, b, c = map(int, input().split())
-    adj[a].append([b, c])
-    adj[b].append([a, c])
+    edges.append([a, b, c])
 
-# 1번 노드부터 시작
-mst = [-1] * (V + 1)
-H = []
-heapq.heappush(H, [0, 1])
+# 가장 작은 가중치의 간선부터 연결하기 위해 가중치 기준 오름차순 정렬
+edges.sort(key = lambda x: x[2])
 
-while H:
-    power, node = heapq.heappop(H)
-    if mst[node] != -1: continue
+# 부모를 찾는 함수
+def find(parent, x):
+    if parent[x] != x:
+        return find(parent, parent[x])
+    return parent[x]
+
+# 더 부모 합치기
+def union(parent, a, b):
+    a, b = find(parent, a), find(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+        
+parent = [0] * (V + 1)
+for i in range(V + 1):
+    parent[i] = i
+
+ans_cost = 0
+
+for s, e, cost in edges:
+    # 부모가 같은 경우 (사이클이 이어지는 경우) -> 간선을 이으면 안됨
+    if find(parent, s) == find(parent, e): continue
     
-    mst[node] = power
-    for next_node, get_power in adj[node]:
-        heapq.heappush(H, [get_power, next_node])
+    # 부모가 다른 경우는 간선 이어주고 가중치 ++
+    union(parent, s, e)
+    ans_cost += cost
 
-print(sum(mst[1:]))
+print(ans_cost)
