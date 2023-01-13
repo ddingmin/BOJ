aac2b89e01b68dbb3542d41f85adaa7dbd1c688c
ddingmin <ddingminyt@gmail.com>
2023-01-13 22:14:11 +0900

    [unrated] Title: 개인정보 수집 유효기간, Time: 0.15 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
@@ -1,31 +1,36 @@
-def check_answer(limit_date, today):
-    del_info = []
-    ty, tm, td = map(int, today.split('.'))
-    tm += ty * 12
-    td += tm * 28
-    
-    for cur in limit_date:
-        num, d = cur
-        if td >= d: del_info.append(num)
-    return del_info
+def hashing(date):
+    y, m, d = map(int, date.split('.'))
+    m += y * 12
+    d += m * 28
+    return d
 
-def solution(today, terms, privacies):
-    answer = []
-    # 약관 종류 정리
+# 약관의 종류를 딕셔너리로 정리
+def get_rules(terms):
     rules = {}
     for cur in terms:
         a, b = cur.split()
-        rules[a] = int(b)
-        
+        rules[a] =int(b)
+    return rules
+
+def check_answer(end_dates, today):
+    target = hashing(today)
+    ans = []
+    for index, value in end_dates:
+        if target >= value:
+            ans.append(index)
+    return ans
+def solution(today, terms, privacies):
+    answer = []
+    # 약관 종류 정리
+    rules = get_rules(terms)
+
     # 파기 만료 기간 구하기
-    dates = []
+    end_dates = []
     for i in range(len(privacies)):
         date, r = privacies[i].split()
-        y, m, d = map(int, date.split('.'))
-        
-        m += y * 12 + rules[r]
-        d += m * 28
-        dates.append([i + 1, d])
-    answer = check_answer(dates, today)
+        value = hashing(date)
+        value += rules[r] * 28
+        end_dates.append([i + 1, value])
+    answer = check_answer(end_dates, today)
 
     return answer
