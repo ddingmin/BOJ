72a4cfcbb765505b91b662a92efaa68952dbbfa9
ddingmin <ddingminyt@gmail.com>
2023-02-22 14:58:30 +0900

    [Gold V] Title: 최소비용 구하기, Time: 648 ms, Memory: 139628 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
@@ -1,29 +1,24 @@
-import sys
 import heapq
 
-# sys.setrecursionlimit(100001)
-input = __import__('sys').stdin.readline
-
-# Input
+def solve(n, adj, start, end):
+    hq = []
+    visit = [float('inf')] * (n + 1)
+    heapq.heappush(hq, [0, start])
+    visit[start] = 0
+    while hq:
+        cur_cost, cur_node = heapq.heappop(hq)
+        if visit[cur_node] != cur_cost: continue
+        for next_node, next_cost in adj[cur_node]:
+            if visit[next_node] > cur_cost + next_cost:
+                visit[next_node] = cur_cost + next_cost
+                heapq.heappush(hq, [cur_cost + next_cost, next_node])
+    return visit[end]
+    
 n = int(input())
 m = int(input())
-adj = [[] for _ in range(n + 1)] # 인접리스트의 뼈대
-
+adj = [[] for _ in range(n + 1)]
 for _ in range(m):
-    a, b, c = map(int, input().split())
-    adj[a].append([b, c]) # 단방향 연결
-start, dest = map(int, input().split())
-
-cost = [float('inf')] * (n + 1)
-hq = [[0, start]] # 시작 노드 넣어주기 [간선의 비용, 간선]
-cost[start] = 0 # 시작 노드 -> 시작 노드 가는 비용은 0
-
-while hq:
-    t, x = heapq.heappop(hq) # t: 현재까지의 가중치, x는 현재 노드
-    if cost[x] != t: continue
-    for nx, nt in adj[x]: # nx는 노드, nt는 가중치
-        if cost[nx] > t + nt:
-            cost[nx] = t + nt # nx비용 초기화
-            heapq.heappush(hq, [cost[nx], nx])
-
-print(cost[dest])
+    s, e, cost = map(int, input().split())
+    adj[s].append([e, cost])
+start, end = map(int, input().split())
+print(solve(n, adj, start, end))
