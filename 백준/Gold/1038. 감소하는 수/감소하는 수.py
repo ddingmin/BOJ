46e98b352bb416e1cc55367a74db454cbfd5a7db
ddingmin <ddingminyt@gmail.com>
2023-02-28 14:08:11 +0900

    [Gold V] Title: 감소하는 수, Time: 132 ms, Memory: 115268 KB -BaekjoonHub

--- 백준/Gold/1038. 감소하는 수/감소하는 수.py
+++ 백준/Gold/1038. 감소하는 수/감소하는 수.py
@@ -1,30 +1,18 @@
-input = __import__('sys').stdin.readline
+def solve(n):
+    answer = []
+    def dfs(depth, num):
+        answer.append(int(num))
+        for i in range(9, -1, -1):
+            if num[-1] > str(i):
+                dfs(depth + 1, num + str(i))
+    
+    for i in range(10):
+        dfs(0, str(i))
+    
+    answer.sort()
+    if n < len(answer): return answer[n]
+    else: return -1
 
-# input
+
 n = int(input())
-
-
-# 해당 배열이 감소하는 수 인지 확인
-def check(i):
-	global num
-	if len(num) == 1: return 1
-	if num[-2] > i: return 1
-	else: return 0
-
-num = []
-ans = []
-def dfs(depth):
-	global num
-	for i in range(10):
-		num.append(i)
-		if check(i):
-			dfs(depth + 1)
-			ans.append(int(''.join(str(x) for x in num)))
-		num.pop()
-	
-dfs(0)
-ans.sort()
-
-# 감소하는 수는 1022개 존재 그외는 -1 출력
-if n >= len(ans): print(-1)
-else: print(ans[n])
+print(solve(n))
