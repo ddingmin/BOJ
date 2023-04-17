279c66d01bd63df2961bb60cb958011713a9aad5
ddingmin <ddingminyt@gmail.com>
2023-04-17 18:20:30 +0900

    [Gold V] Title: 4와 7, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Gold/2877. 4와 7/4와 7.py
+++ 백준/Gold/2877. 4와 7/4와 7.py
@@ -1,13 +1,1 @@
-import sys
-import math
-input = sys.stdin.readline
-
-k = int(input())
-
-def solve(k):
-    target = int(math.log2(k + 1))
-    for i in range(1, target):
-        k -= 2 ** i
-    k -= 1
-    return bin(k)[2:].replace('0', '4').replace('1', '7').rjust(target, '4')
-print(solve(k))
+print(bin(int(input())+1)[3:].replace('0','4').replace('1','7'))
