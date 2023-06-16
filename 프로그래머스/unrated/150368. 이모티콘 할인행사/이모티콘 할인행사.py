9de1206a3b2f0b5bcbda1a6daddaae9037955f4c
ddingmin <ddingminyt@gmail.com>
2023-06-16 19:17:03 +0900

    [unrated] Title: 이모티콘 할인행사, Time: 0.12 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/150368. 이모티콘 할인행사/이모티콘 할인행사.py
+++ 프로그래머스/unrated/150368. 이모티콘 할인행사/이모티콘 할인행사.py
@@ -1,9 +1,45 @@
+
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
 def solution(users, emoticons):
     answer = [0, 0]
     data = [10 ,20, 30, 40]
     discount = []
-    
-    # 이모티콘 할인율 구하기
     def dfs(temp, depth):
         if depth == len(temp):
             discount.append(temp[:])
@@ -12,31 +48,28 @@ def solution(users, emoticons):
             temp[depth] += d
             dfs(temp, depth + 1)
             temp[depth] -= d
-    
+
     dfs([0] * len(emoticons), 0)
-    
-    # 완전탐색
+
     for d in range(len(discount)):
         join, price = 0, [0] * len(users)
         for e in range(len(emoticons)):
             for u in range(len(users)):
-                # 할인율을 만족하면 구매
                 if users[u][0] <= discount[d][e]:
                     price[u] += emoticons[e] * (100 - discount[d][e]) / 100
-        
-        # 구매 금액에 따라 가입자 갱신
+        # print(price)
         for u in range(len(users)):
             if price[u] >= users[u][1]:
                 join += 1
                 price[u] = 0
-        
-        # 최대 가입자, 구매 금액 갱신
+
+        # 갱신
         if join >= answer[0]:
             if join == answer[0]:
                 answer[1] = max(answer[1], sum(price))
             else:
                 answer[1] = sum(price)
             answer[0] = join
-    
-    
+
+
     return answer
