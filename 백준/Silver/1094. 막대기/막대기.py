bd9a09022f49cc52f5ef7cc828d3541f0343e5f4
ddingmin <ddingminyt@gmail.com>
2023-09-16 22:08:31 +0900

    [Silver V] Title: 막대기, Time: 148 ms, Memory: 114436 KB -BaekjoonHub

---
+++ 백준/Silver/1094. 막대기/막대기.py
@@ -0,0 +1,21 @@
+import sys
+import math
+from collections import deque
+
+input = sys.stdin.readline
+
+
+x = int(input())
+
+stick = [64, 32, 16, 8, 4, 2, 1]
+count = 0
+
+for i in range(len(stick)):
+    if x >= stick[i]:
+        count += 1
+        x -= stick[i]
+
+    if x == 0:
+        break
+
+print(count)
