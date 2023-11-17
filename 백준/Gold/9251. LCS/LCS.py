16392ca1f856d19242ee61293c95a2ca6532f788
ddingmin <ddingminyt@gmail.com>
2023-11-18 00:49:33 +0900

    [Gold V] Title: LCS, Time: 508 ms, Memory: 55712 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.py
@@ -0,0 +1,12 @@
+input = __import__('sys').stdin.readline
+
+a = list(input().strip())
+b = list(input().strip())
+
+dp = [[0] * (len(b) + 1)  for _ in range(len(a)+1)]
+
+for i in range(1,len(a)+1):
+    for j in range(1,len(b)+1):
+        if a[i-1] == b[j-1]: dp[i][j] = dp[i-1][j-1] + 1
+        else: dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+print(dp[-1][-1])
