6abde255a9e81e38d5797aa84cf3572af9584e42
ddingmin <ddingminyt@gmail.com>
2024-01-14 04:30:09 +0900

    [Bronze V] Title: 2 桁の整数 (Two-digit Integer), Time: 108 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/27331. 2 桁の整数 （Two－digit Integer）/2 桁の整数 （Two－digit Integer）.py
@@ -0,0 +1,17 @@
+import sys
+
+
+def solve(arr):
+    pass
+
+
+def main():
+    a = int(input()) * 10
+    b = int(input())
+    print(a + b)
+
+
+if __name__ == '__main__':
+    # sys.setrecursionlimit(10 ** 6)
+    input = sys.stdin.readline
+    main()
