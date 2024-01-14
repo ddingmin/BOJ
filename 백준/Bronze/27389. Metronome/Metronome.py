8c5ccaae51da921f0d836f2cbed60c90fa2b7b8a
ddingmin <ddingminyt@gmail.com>
2024-01-14 22:39:11 +0900

    [Bronze V] Title: Metronome, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/27389. Metronome/Metronome.py
@@ -0,0 +1,15 @@
+import sys
+
+
+def solve(arr):
+    pass
+
+
+def main():
+    ans = int(input()) / 4
+    print(f"{ans}")
+
+if __name__ == '__main__':
+    # sys.setrecursionlimit(10 ** 6)
+    input = sys.stdin.readline
+    main()
