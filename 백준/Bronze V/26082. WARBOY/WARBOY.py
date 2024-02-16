4afff5e5c4fb65e44edee4bb91743ef0ade7ece1
ddingmin <ddingminyt@gmail.com>
2024-02-17 00:42:06 +0900

    [Bronze V] Title: WARBOY, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze V/26082. WARBOY/WARBOY.py
@@ -0,0 +1,16 @@
+import sys
+
+
+def solve(n, m, visit, adj):
+    pass
+
+
+def main():
+    a, b, c = map(int, input().split())
+    print(int(b / a * 3 * c))
+
+
+if __name__ == '__main__':
+    # sys.setrecursionlimit(10 ** 6)
+    input = sys.stdin.readline
+    main()
