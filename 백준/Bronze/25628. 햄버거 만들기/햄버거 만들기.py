e84f6e3c28ff354ad33a4d2ee30626daea5bbb56
ddingmin <ddingminyt@gmail.com>
2024-02-21 20:59:46 +0900

    [Bronze IV] Title: 햄버거 만들기, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/25628. 햄버거 만들기/햄버거 만들기.py
@@ -0,0 +1,16 @@
+import sys
+
+
+def solve(n, m, visit, adj):
+    pass
+
+
+def main():
+    a, b = map(int, input().split())
+    print(min(a // 2, b))
+
+
+if __name__ == '__main__':
+    # sys.setrecursionlimit(10 ** 6)
+    input = sys.stdin.readline
+    main()
