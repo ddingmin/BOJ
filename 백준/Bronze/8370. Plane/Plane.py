830367dbf3abd139b9526c8791fe97e38423bc68
ddingmin <ddingminyt@gmail.com>
2024-02-23 01:26:44 +0900

    [Bronze V] Title: Plane, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/8370. Plane/Plane.py
@@ -0,0 +1,16 @@
+import sys
+
+
+def solve(n, m, visit, adj):
+    pass
+
+
+def main():
+    a, b, c, d = map(int, input().split())
+    print(a * b + c * d)
+
+
+if __name__ == '__main__':
+    # sys.setrecursionlimit(10 ** 6)
+    input = sys.stdin.readline
+    main()
