2810df127948f088146d6fe89f06c8ad7c10ff62
ddingmin <ddingminyt@gmail.com>
2024-02-25 22:49:02 +0900

    [Silver V] Title: 투자의 귀재 배주형, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Silver/19947. 투자의 귀재 배주형/투자의 귀재 배주형.py
@@ -0,0 +1,25 @@
+import sys
+
+
+def solve(arr):
+    pass
+
+
+def main():
+    h, y = map(int, input().split())
+    dp = [0] * (y + 1)
+    dp[0] = h
+
+    for i in range(1, y + 1):
+        if i >= 5:
+            dp[i] = int(max(dp[i - 1] * 1.05, dp[i - 3] * 1.2, dp[i - 5] * 1.35))
+        elif i >= 3:
+            dp[i] = int(max(dp[i - 1] * 1.05, dp[i - 3] * 1.2))
+        else:
+            dp[i] = int(dp[i - 1] * 1.05)
+    print(dp[y])
+
+
+if __name__ == '__main__':
+    input = sys.stdin.readline
+    main()
