9f5b01678252c7194bed77709d7a306c5ac30534
ddingmin <ddingmin.dev@gmail.com>
2024-04-23 23:58:46 +0900

    [Bronze IV] Title: 빵, Time: 116 ms, Memory: 108080 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/25377. 빵/빵.py
+++ 백준/Bronze/25377. 빵/빵.py
@@ -1,6 +1,12 @@
 ans = float('inf')
 n = int(input())
+
 for _ in range(n):
     a, b = map(int, input().split())
+    if a > b: continue
     ans = min(ans, max(a, b))
-print(ans)
+
+if ans == float('inf'):
+    print(-1)
+else:
+    print(ans)
