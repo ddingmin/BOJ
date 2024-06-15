426c907abd8840b13ff212c12e36a227b2b4d85d
ddingmin <ddingmin.dev@gmail.com>
2024-06-16 02:12:25 +0900

    [Silver III] Title: 순열 사이클, Time: 300 ms, Memory: 239812 KB -BaekjoonHub

---
+++ 백준/Silver/10451. 순열 사이클/순열 사이클.py
@@ -0,0 +1,21 @@
+import sys
+sys.setrecursionlimit(10 ** 5)
+
+def dfs(x):
+    visited[x] = True 
+    number = numbers[x] 
+    if not visited[number]: 
+        dfs(number) #재귀
+
+for _ in range(int(input())):
+    N = int(input())
+    numbers = [0] + list(map(int, input().split()))
+    visited = [True] + [False] * N
+    result = 0
+    
+    for i in range(1, N+1):
+        if not visited[i]: 
+            dfs(i) 
+            result += 1 
+    print(result)
+    
