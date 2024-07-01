b2d33aeeade57b9306c5da0b627c07779d35c4d1
ddingmin <ddingmin.dev@gmail.com>
2024-07-01 23:00:02 +0900

    [Bronze III] Title: 삼각형과 세 변, Time: 88 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/5073. 삼각형과 세 변/삼각형과 세 변.py
@@ -0,0 +1,14 @@
+while True:
+    a, b, c = map(int, input().split())
+    if a == b == c == 0:
+        break
+
+    if a == b == c:
+        print("Equilateral")
+    elif 2 * max(a, b, c) >= a + b + c:
+        print("Invalid")
+    elif a == b or b == c or a == c:
+        print("Isosceles")
+    else:
+        print("Scalene")
+        
