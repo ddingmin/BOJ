9cbc4c00af36a23a09be76fa62b690e18a4baef3
ddingmin <ddingmin.dev@gmail.com>
2024-07-07 03:29:39 +0900

    [Bronze III] Title: TGN, Time: 120 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/5063. TGN/TGN.py
@@ -0,0 +1,10 @@
+T = int(input())
+
+for _ in range(T):
+    r, e, c = map(int, input().split())
+    if r > e - c:
+        print('do not advertise')
+    elif r == e - c:
+        print('does not matter')
+    else:
+        print('advertise')
