6e30402c54b67981279fe68258d0afce022292ca
ddingmin <ddingmin.dev@gmail.com>
2024-07-08 22:01:37 +0900

    [Bronze II] Title: 저항, Time: 96 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/1076. 저항/저항.py
@@ -0,0 +1,7 @@
+color = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white']
+
+a = str(color.index(input()))
+b = str(color.index(input()))
+c = str(10**color.index(input()))
+
+print(int(a+b+c[1:]))
