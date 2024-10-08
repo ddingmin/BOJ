187853cd0eb93ca9ffb6a7a89dfead12a4872a79
ddingmin <ddingmin.dev@gmail.com>
2024-10-09 01:36:45 +0900

    [Bronze I] Title: 슈퍼 마리오, Time: 84 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/2851. 슈퍼 마리오/슈퍼 마리오.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+m = []
+score = 0
+for i in range(10):
+    m.append(int(input()))
+for j in m:
+    score += j
+    if score >= 100:
+        if score - 100 > 100 - (score - j):
+            score -= j
+        break
+print(score)
